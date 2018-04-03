6303bc88b3f00089ae5c3f8557a2be6653b66e17
Diana Suvorova <diana.suvorova@uber.com>
2018-04-03 13:35:36 -0700

    test: adding script to run e2e on travis

--- src/e2e.test.js
+++ src/e2e.test.js
@@ -1,10 +1,12 @@
 import puppeteer from 'puppeteer';
 
+const PORT = process.env.PORT || '3000'
+
 test('can access enzyme el for e2e tests', async () => {
   const browser = await puppeteer.launch({});
   const page = await browser.newPage();
 
-  await page.goto('http://localhost:3000', {waitUntil: 'networkidle2'});
+  await page.goto(`http://localhost:${PORT}`, {waitUntil: 'networkidle2'});
 
   const appName = await page.evaluate(() => {
     app = window.el.findWhere(c => c.name() === 'App');
